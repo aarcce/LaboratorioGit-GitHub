33b9b3d06b902e884178a7ed8ebb9da3161983cb
Juan Diego Arce <juan.arcenavarro@ucr.ac.cr>
2023-12-05 15:12:33 -0600

    Se aplica un prettier de formato al archivo del lab 2

--- laboratorio2.c
+++ laboratorio2.c
@@ -4,21 +4,25 @@
 // Nombre: Juan Diego Arce
 // Laboratorio 2
 
-int calcularMCD(int a, int b) {
+int calcularMCD(int a, int b)
+{
     int n;
 
     // Se calcula el máximo común divisor
-    while (b != 0) {
+    while (b != 0)
+    {
         n = b;
         b = a % b;
         a = n;
     }
     return a;
 }
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
     // Verifica la cantidad correcta de argumentos
-    if (argc != 3) {
+    if (argc != 3)
+    {
         printf("Error: Debe proporcionar exactamente dos números enteros.\n");
         return 1;
     }
@@ -27,12 +31,14 @@ int main(int argc, char *argv[]) {
     int num2 = atoi(argv[2]);
 
     // Se verifica que los números no sean negativos
-    if (num1 < 0 || num2 < 0) {
+    if (num1 < 0 || num2 < 0)
+    {
         printf("Error: Los números deben ser no negativos.\n");
         return 1;
     }
     // Se verifica que los numeros no sean igual a cero
-    if (num1 == 0 && num2 == 0) {
+    if (num1 == 0 && num2 == 0)
+    {
         printf("Error: Ambos números no pueden ser cero.\n");
         return 1;
     }
